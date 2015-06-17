a655097a9010daafb5b4e25afb1a8b2e0ec1602b
russleggett <lrl005@bucknell.edu>
2015-06-17 17:16:29 -0400

    fix

--- app/models/approver.rb
+++ app/models/approver.rb
@@ -1,5 +1,5 @@
 class Approver < ActiveRecord::Base
-	has_one :user class_name: 'User', foreign_key: :user_id
+	has_one :user, class_name: 'User', foreign_key: :user_id
 	has_one :supervisor. class_name: 'User', foreign_key: :their_supervisor
 	has_many :approvals
 end
